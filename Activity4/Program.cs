61ea726c84013d7fa57907363bc6c4c62ed2041f
Dean Justine Suficiencia <djsuficiencia@csmbataan.ph.education>
2024-10-18 20:09:17 +0800

    Act 4

---
+++ Activity4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        // Conversion rate from USD to EUR
+        double conversionRate = 0.85;
+
+        // Asking user to enter an amount in USD
+        Console.Write("Enter the amount in USD: ");
+        double usd = Convert.ToDouble(Console.ReadLine());
+
+        // Convert USD to EUR
+        double eur = usd * conversionRate;
+
+        // Display the result
+        Console.WriteLine($"{usd} USD is equivalent to {eur} EUR.");
+    }
+}
